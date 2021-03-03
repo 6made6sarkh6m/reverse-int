3a53935b01b68bbc260f4e059e4f77925c35f6b3
Temirlan <amadeusarkhammorningstar@gmail.com>
2021-03-03 14:16:16 +0600

    reverse integer solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+    if(n<0){
+        n = n * -1;
+    }
+    let res = [];
   
+    
+    while(n>0){
+      let v = n%10;
+      res.push(v);
+      n = parseInt(n/10);
+    }
+    res = parseInt(res.join(''))
+    return res;
 }
